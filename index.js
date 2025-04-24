58a1c4cea5f40ccfa7d34d6d3b09f18d30dc1acb
gezzzo <gezzzo30@gmail.com>
2025-04-24 18:06:00 +0200

    updates

--- index.js
+++ index.js
@@ -27,11 +27,18 @@ const upload = multer({ storage });
 
 const checkPasskey = (req, res, next) => {
     const passkey = req.header('x-passkey'); 
-    if (!passkey || passkey != 'Z3xkP8kqXQ9cM2yNzF6vL!aI4tRz1bPb7JkC9uYdS#Z5wD6') { 
+    if (!passkey || passkey != 'dr-gezo-1-2000-2-2025-darsh') { 
         return res.status(403).json({ message: 'Forbidden: Invalid passkey' });
     }
     next(); 
 };
+
+
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Welcome' });
+});
+
 
 app.use('/media', express.static(MEDIA_DIR));
 
